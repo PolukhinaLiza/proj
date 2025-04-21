7c9c1a0147a072d8c93f47d461b79300883b87b5
Nazarova Elizaveta <polukhina.liza.01@mail.ru>
2025-04-21 09:49:35 +0300

    Изменен метод

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,9 +3,11 @@ package com.stepup.proj;
 public class MainApplication {
     public  static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("11111");
+        System.out.println("11001");
     }
     public static void doSomething() {
-        System.out.println();
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
